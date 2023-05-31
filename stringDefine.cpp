f43600ae4bcd4a9ba6110f6b684a04531a7321de
srayo16 <srayosikder@gmail.com>
2023-05-31 13:16:10 +0600

    first commit

---
+++ stringDefine.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Srayo Sikder";
+
+    string a("Srayo");
+    string b("Srayo", 2);
+    string c(100, 's');
+    cout << c;
+
+    return 0;
+}
